9a3870046b87c1e4e5f2d021f5b7bb976b316af2
camille-s <camille.seaberry@gmail.com>
2024-04-19 17:12:35 -0400

    Improve topo shapes, reference all in index file

---
+++ src/data/shapes/index.js
@@ -0,0 +1,13 @@
+import bridgeport from './bridgeport_topo.json';
+import hartford from './hartford_topo.json';
+import new_haven from './new_haven_topo.json';
+import stamford from './stamford_topo.json';
+
+const shapes = {
+    bridgeport,
+    hartford,
+    new_haven,
+    stamford,
+};
+
+export default shapes;
